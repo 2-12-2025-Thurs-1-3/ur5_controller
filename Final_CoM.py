07920e7aeca9f2392edafba137acf39f2430f37e
hlugaro <hlugaro@mit.edu>
2025-05-01 19:21:44 -0400

    Grab bottle and check if holding!

---
+++ Final_CoM.py
@@ -0,0 +1,204 @@
+# Final version of the CV
+import numpy as np
+import cv2  # OpenCV module
+import time
+from tkinter import *
+import speed_moveL as control
+
+# Constants and other global variables
+CAMERAID = 0 # CHANGE AS NECESSARY
+camera = cv2.VideoCapture(CAMERAID) # define camera
+colorID = [0,0,0,0]
+COLORS = [(0,255,120),(255,120,120),(0,69,255),(255,255,255)]
+POINTS = [(.69154,-.600138),(.67307,-.135855),(.1763488,-.1238),(.13237,-.63746)] # Calibrate from robot
+MYPOINTS = [(33,403),(350,400),(370,67),(25,25)] # Equivalent in pixels
+TOPCUTOFF = 50 # How far from top before sending data
+BOTTCUTOFF = 300 # How far from bottom before beginning data collection
+DT=.05 
+src = np.array(MYPOINTS)
+dst = np.array(POINTS)
+
+#Threshold Values
+# yellow 
+lower_bound_HSV_yellow = np.array([21, 39, 103]) 
+upper_bound_HSV_yellow = np.array([36, 255, 255])
+# blue 
+lower_bound_HSV_blue = np.array([71, 135, 68])
+upper_bound_HSV_blue = np.array([105, 255, 255])
+# orange 
+lower_bound_HSV_orange = np.array([13, 90, 200])
+upper_bound_HSV_orange = np.array([25, 255, 255])
+thresholds = [[lower_bound_HSV_yellow,upper_bound_HSV_yellow],
+              [lower_bound_HSV_blue,upper_bound_HSV_blue],
+               [lower_bound_HSV_orange,upper_bound_HSV_orange],
+               [np.array([255,255,255]),np.array([255,255,255])]]
+
+#Using values, calculate H (one plane to another)
+x, y, u, v = src[:,0], src[:,1], dst[:,0], dst[:,1]
+A = np.zeros((9,9))
+j = 0
+for i in range(4):
+    A[j,:] = np.array([-x[i], -y[i], -1, 0, 0, 0, x[i]*u[i], y[i]*u[i], u[i]])
+    A[j+1,:] = np.array([0, 0, 0, -x[i], -y[i], -1, x[i]*v[i], y[i]*v[i], v[i]])
+    j += 2
+A[8, 8] = 1   # assuming h_9 = 1
+b = [0]*8 + [1]
+H = np.reshape(np.linalg.solve(A, b), (3,3))
+#print(H)
+
+#Defining Bottle Class
+class Bottle:
+    def __init__(self,clr: int, pos: list,rpos: list):
+        self.color = clr
+        self.pos = [pos]
+        self.time = [time.time()]
+        self.velX = 0
+        self.velY = 0
+        self.future = [0 for _ in range(100)]
+        self.robot_pos=[rpos]
+    def update_pos(self,new_pos: list, rob_pos: list):
+        self.pos.append(new_pos)
+        self.time.append(time.time())
+        self.robot_pos.append(rob_pos)
+    def find_velocity(self):
+        self.velX = (self.pos[-1][0]-self.pos[0][0])/(self.time[-1]-self.time[0])
+        self.velY = (self.pos[-1][1]-self.pos[0][1])/(self.time[-1]-self.time[0])
+    def future_pos(self):
+        dT = DT
+        curr_time = time.time()
+        for i in range(100):
+            self.future[i] = (self.pos[-1][0]+self.velX*dT*i,self.pos[-1][1]+self.velY*dT*i,curr_time+dT*i)
+    def send_data(self):
+        rvelX = (self.robot_pos[-1][0]-self.robot_pos[0][0])/(self.time[-1]-self.time[0])
+        rvelY = (self.robot_pos[-1][1]-self.robot_pos[0][1])/(self.time[-1]-self.time[0])
+        distance = abs(self.robot_pos[-1][0]+.3)
+        self.exp_time = distance/(abs(rvelY))
+        return (self.exp_time, self.color, self.robot_pos[-1][0])
+        print(self.exp_time, self.color, self.robot_pos[-1][0])
+
+def find_contours(image):
+    contoursss, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    min_area = 2500
+    return [cnt for cnt in contoursss if (cv2.contourArea(cnt) > min_area and cv2.contourArea(cnt) < 9000)]
+
+#Once bottle initiated, track just that one color
+def track_one(bottle):
+    cv2.destroyAllWindows()
+    kernel = np.ones((7,7),np.uint8)
+    num_iterations = 3
+    bru=0
+    while True:
+        bru = bru+1
+        ret, cv_image1 = camera.read()
+        cv_image = cv_image1[0:440,138:540]
+        hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+        if bottle.color!=3:
+            mask_HSV = cv2.inRange(hsv_image, thresholds[bottle.color][0], thresholds[bottle.color][1])
+            opening = cv2.morphologyEx(mask_HSV, cv2.MORPH_OPEN, kernel, iterations = num_iterations)
+        else:
+            cannyIm = cv2.Canny(cv_image, 50, 150, apertureSize = 3)
+            opening = cv2.morphologyEx(cannyIm, cv2.MORPH_CLOSE, kernel, iterations = num_iterations)
+        contours = find_contours(opening)
+        cv2.drawContours(cv_image, contours, -1, COLORS[bottle.color],3)
+        if (len(contours)==1):
+            # Compute the center of the contour
+            M = cv2.moments(contours[0])
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            # # Draw the contour and center of the shape on the image (our viewing)
+            cv2.circle(cv_image, (cX, cY), 7, (255, 255, 255), -1)
+            cv2.putText(cv_image, "center", (cX - 20, cY - 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            new = (H[0,0]*cX+H[0,1]*cY+H[0,2],H[1,0]*cX+H[1,1]*cY+H[1,2])
+            if cY<TOPCUTOFF:
+                if bru<5:
+                    break
+                grab_time, color, y_pos = bottle.send_data()
+                break
+            elif cY>BOTTCUTOFF:
+                break
+        else:
+            break
+
+        bottle.update_pos((cX,cY),new)
+        bottle.find_velocity()
+        bottle.future_pos()
+        
+        for i in range(1, len(bottle.future)):
+            pt1 = tuple(map(int, bottle.future[i - 1][0:2]))
+            pt2 = tuple(map(int, bottle.future[i][0:2]))
+            cv2.line(cv_image, pt1, pt2, COLORS[bottle.color], 2)
+        
+        cv2.imshow("Original",cv_image)
+        if bottle.color == 0:
+            cv2.imshow("Opening - Yellow", opening)
+        if bottle.color == 1:
+            cv2.imshow("Opening - Blue", opening)
+        if bottle.color == 2:
+            cv2.imshow("Opening - Orange", opening)
+        if bottle.color == 3:
+            cv2.imshow("Opening - Clear", opening)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+# The main loop of the function
+def detect():
+    # Open up the webcam
+    while True:
+        # Read from the camera frame by frame and crop
+        ret, cv_image1 = camera.read()
+        cv_image = cv_image1[0:440,138:540]
+        hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+        
+        # create four color masks
+        mask_HSV_yellow = cv2.inRange(hsv_image, lower_bound_HSV_yellow, upper_bound_HSV_yellow)
+        mask_HSV_blue = cv2.inRange(hsv_image, lower_bound_HSV_blue, upper_bound_HSV_blue)
+        mask_HSV_orange = cv2.inRange(hsv_image, lower_bound_HSV_orange, upper_bound_HSV_orange)
+        cannyIm = cv2.Canny(cv_image, 50, 150, apertureSize = 3)
+
+        kernel = np.ones((7,7),np.uint8)
+        num_iterations = 3
+
+        ################ Color Detection ####################
+        opening_yellow = cv2.morphologyEx(mask_HSV_yellow, cv2.MORPH_OPEN, kernel, iterations = num_iterations)
+        opening_blue = cv2.morphologyEx(mask_HSV_blue, cv2.MORPH_OPEN, kernel, iterations = num_iterations)
+        opening_orange = cv2.morphologyEx(mask_HSV_orange, cv2.MORPH_OPEN, kernel, iterations = num_iterations)
+        cn_clear = cv2.morphologyEx(cannyIm, cv2.MORPH_CLOSE, kernel, iterations = num_iterations)
+
+        images = [opening_yellow,opening_blue,opening_orange,cn_clear]
+
+        for img in range(len(images)):
+            contours = find_contours(images[img])
+            if len(contours)==1:
+                # Compute the center of the contour
+                M = cv2.moments(contours[0])
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                # # Draw the contour and center of the shape on the image (our viewing)
+                cv2.circle(cv_image, (cX, cY), 7, (255, 255, 255), -1)
+                cv2.putText(cv_image, "center", (cX - 20, cY - 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                new = (H[0,0]*cX+H[0,1]*cY+H[0,2],H[1,0]*cX+H[1,1]*cY+H[1,2])
+                if cY<BOTTCUTOFF and cY>TOPCUTOFF+50:
+                    track = Bottle(img,(cX,cY),new)
+                    track_one(track)
+
+        #cv2.circle(cv_image, (33,403), 7, (255, 255, 255), -1)
+        #cv2.circle(cv_image, (350,400), 7, (255, 255, 255), -1)
+        #cv2.circle(cv_image, (370,67), 7, (255, 255, 255), -1)
+        #cv2.circle(cv_image, (25,25), 7, (255, 255, 255), -1)
+
+        ## display image
+        cv2.imshow("Original",cv_image)
+        cv2.imshow("Canny_Image", cannyIm)
+        cv2.imshow("Opening - Yellow", opening_yellow)
+        cv2.imshow("Opening - Blue", opening_blue)
+        cv2.imshow("Opening - Orange", opening_orange)
+        cv2.imshow("Opening - Clear", cn_clear)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+if __name__=='__main__':
+    detect()
--- speed_moveL.py
+++ speed_moveL.py
@@ -9,11 +9,14 @@ rtde_c = rtde_control.RTDEControlInterface("192.168.1.103")
 portName = '/dev/ttyACM1'
 
 ## Bin Positions (x, y, z, rx, ry, rz)
-positions = {"yellow": [0.11874185587583301, -0.7891758751405292, -0.025397598041483435, -2.341806743327856, 2.0681131911584973, -0.03818426020594277],
-             "blue": [-0.1525171470714167, -0.8030085393136451, -0.02541064764556142, -2.3417794446943234, 2.0680696902802116, -0.03818361678440171],
-             "green": [-0.41902641634478266, -0.7951756441194379, -0.025368432165289723, -2.3417937824894195, 2.068177972269544, -0.038187638517122306],
-             "colab": [0.36197288587652526, -0.7951662181724354, -0.025394826729175105, -2.3417869726414136, 2.0681194592170717, -0.038135188173779394],
-             "neutral": [0.015466521999787283, -0.4899471595439902, 0.13258818072260725, -2.3417131655824277, 2.0681925358661806, -0.03821438995733724]}
+positions = {"yellow": [0.11874185587583301, -0.7891758751405292, 0.125397598041483435, -2.341806743327856, 2.0681131911584973, -0.03818426020594277],
+             "blue": [-0.1525171470714167, -0.8030085393136451, 0.12541064764556142, -2.3417794446943234, 2.0680696902802116, -0.03818361678440171],
+             "green": [-0.41902641634478266, -0.7951756441194379, 0.125368432165289723, -2.3417937824894195, 2.068177972269544, -0.038187638517122306],
+             "colab": [0.36197288587652526, -0.7951662181724354, 0.125394826729175105, -2.3417869726414136, 2.0681194592170717, -0.038135188173779394],
+             "neutral": [0.015466521999787283, -0.4899471595439902, 0.23258818072260725, -2.3417131655824277, 2.0681925358661806, -0.03821438995733724],
+             "standby": [-0.29992314772168255, -0.4899471595439902, 0.05152892504111392,  -2.3417131655824277, 2.0681925358661806, -0.03821438995733724]
+
+}
 
 ## Ideal Bounding Box (During gripping)
 bounding_box = {"max": [-0.39074419281582623, -0.7766767888560709, 0.2364383950071206, -2.2125974178272223, -2.2092962831368443, 0.13271170247340863],
@@ -26,21 +29,6 @@ close = [0.17345416286195572, -0.22936740746653966, 0.02083577751346813, 2.65081
 def vector_magnitude(delta_pose):
     return (sum([d*d for d in delta_pose[:3]]))**0.5  # Only XYZ translation error
 
-## Check if current pose is within bounds.
-def in_bounds(pose):
-    return (pose[0] > bounding_box["max"][0] and pose[0] < bounding_box["min"][0] and
-        pose[1] > bounding_box["max"][1] and pose[1] < bounding_box["min"][1] and
-        pose[2] < bounding_box["max"][2] and pose[2] > bounding_box["min"][2] and
-        vector_magnitude(close) <  vector_magnitude(pose) )
-
-def height_check(pose):
-    return pose[2] > -0.01177168031333517
-
-## Check for contact with bottle.
-def force_check(z_force):
-    current_force = rtde_r.getActualTCPForce()
-    return current_force[2] < z_force
-
 def hold_check():
     start_time = time.time()
     i = 0
@@ -50,12 +38,21 @@ def hold_check():
         total_force += rtde_r.getActualTCPForce()[2]
         serial_port.write(b"on: True \n")
     average_force = total_force/i
-    print(average_force)
-    print(average_force < -2.7)
-    return average_force < -2.7
+    return average_force < -0.15
 
+## Check for contact with bottle.
+def force_check(z_force):
+    current_force = rtde_r.getActualTCPForce()
+    return current_force[2] < z_force
 
-## Go to target (x,y,z) position.
+## Check if current pose is within bounds.
+def in_bounds(pose):
+    return (pose[0] > bounding_box["max"][0] and pose[0] < bounding_box["min"][0] and
+        pose[1] > bounding_box["max"][1] and pose[1] < bounding_box["min"][1] and
+        pose[2] < bounding_box["max"][2] and pose[2] > bounding_box["min"][2] and
+        vector_magnitude(close) <  vector_magnitude(pose) )
+
+# Go to target (x,y,z) position.
 def speed_moveL(target_pose, bounds=False, force=False, vacuum=False):
     move_duration = 0.1
     acceleration = 2.0
@@ -64,18 +61,19 @@ def speed_moveL(target_pose, bounds=False, force=False, vacuum=False):
     pose = rtde_r.getActualTCPPose()
     delta_pose = [target_pose[i] - pose[i] for i in range(6)]
 
-    while vector_magnitude(delta_pose) > tolerance and (in_bounds(pose) or not bounds) and (force_check(10.0) or not force):
+    while vector_magnitude(delta_pose) > tolerance and ((in_bounds(pose) or not bounds) and (force_check(10.0) or not force)):
 
         if vacuum:
             serial_port.write(b"on: True \n")
 
         pose = rtde_r.getActualTCPPose()
-        delta_pose = [target_pose[i] - pose[i] for i in range(6)]
+        delta_pose = [2*(target_pose[i] - pose[i]) for i in range(6)]
 
         # Create speed command: Only XYZ are moving, rotations stay zero
         speed = [0, 0, 0, 0, 0, 0]
         for i in range(3):  # Only for x, y, z
-            speed[i] = max(-0.25, min(0.25, delta_pose[i]))  # Clip speed
+            speed[i] = max(-0.25, min(0.25, delta_pose[i]))  # Clip 
+        
 
         rtde_c.speedL(speed, acceleration, move_duration)
         time.sleep(move_duration)
@@ -83,21 +81,18 @@ def speed_moveL(target_pose, bounds=False, force=False, vacuum=False):
     # Stop the robot
     rtde_c.speedL([0, 0, 0, 0, 0, 0], acceleration, 0)
 
-
-## NEEDS TESTING
-def speed_followL(target_pose, speed):
-    pose = rtde_r.getActualTCPPose()
-    speed = [max(-0.25, min(0.25, target_pose[i] - pose[i])) for i in range(6)] # Task-space P control with a maximum speed threshold and gain Kp = 1
-    rtde_c.speedL(speed, 2, 0)
-    time.sleep(0.1)
+# # NEEDS TESTING
+# def speed_followL(target_pose, speed):
+#     pose = rtde_r.getActualTCPPose()
+#     speed = [max(-0.25, min(0.25, target_pose[i] - pose[i])) for i in range(6)] # Task-space P control with a maximum speed threshold and gain Kp = 1
+#     rtde_c.speedL(speed, 2, 0)
+#     time.sleep(0.1)
 
 ## Throw bottle!
 def throw():
     speed = [0, -1, 0, 0, 0, 0]
     acceleration = 4.0
     move_duration = 0.02  # Move for 20 milliseconds each command
-
-
 
     for i in range(15):
         print(i)
@@ -115,6 +110,31 @@ def throw():
     print("STOP")
     rtde_c.speedL([0, 0, 0, 0, 0, 0], acceleration, 0)  # Stop immediately
 
+def autonomy(y_pos, color, time_to_grab):
+    start_time = time.time()    
+    pickup_time = 0.5
+    speed_moveL(positions["standby"])
+
+    target = pose.copy
+    target[1] = y_pos[1]
+
+    speed_moveL(target, vacuum=True)
+
+    cur_time = time.time()
+    while ( time_to_grab - (cur_time-start_time) - pickup_time > 0):
+        cur_time = time.time()
+        serial_port.write(b"on: True \n")
+        time.sleep(0.1)
+
+    target[2] = -0.012677585773136338
+    rtde_c.zeroFtSensor()
+    speed_moveL(target, force=True, vacuum=True)
+    speed_moveL(positions["neutral"], vacuum=True)
+    if not hold_check():
+        return
+    
+    
+
 
 if __name__ == '__main__':
     # PRINT ENDPOINT POSITION
@@ -124,27 +144,66 @@ if __name__ == '__main__':
 
     serial_port = serial.Serial(port=portName, baudrate=115200, timeout=1, write_timeout=1)
 
-    # target = pose
+    # speed_moveL(positions["standby"])
+    # target = positions["standby"]
+    # target[2] = -0.012677585773136338
+    # speed_moveL(target, vacuum=True, force=True)
+
+    target = pose.copy()
+    target[2] = -0.012677585773136338
+
+    start_time = time.time()
+    while time.time() - start_time < 2:
+        serial_port.write(b"on: True \n")
+        time.sleep(0.25)
+    rtde_c.zeroFtSensor()
+    speed_moveL(target, force=True, vacuum=True)
+    speed_moveL(positions["neutral"], vacuum=True)
+    print(hold_check())
+
+    # target = pose.copy()
     # target[0] += 0.1
     # speed_moveL(target, vacuum=True)
-
-
-    hold_check()
-    hold_check()
-    hold_check()
-    hold_check()
-    # throw()
-    serial_port.close()
+    
+    # start_time = time.time()
+    # while time.time() - start_time < 2:
+    #     serial_port.write(b"on: True \n")
+    #     time.sleep(0.25)
+    # rtde_c.zeroFtSensor()
+    # print("ZEROED")
+    # while time.time() - start_time < 2:
+    #     serial_port.write(b"on: True \n")
+    #     time.sleep(0.25)
+    # print(hold_check())
+    # print(hold_check())
+    # print(hold_check())
+    # print(hold_check())
+    # # throw()
+    # serial_port.close()
 
 
 
     # # GO TO LOCATIONS.
-    # speed_moveL(positions["neutral"])
+    # speed_moveL(positions["standby"])
     # speed_moveL(positions["green"])
     # speed_moveL(positions["blue"])
     # speed_moveL(positions["yellow"])
     # speed_moveL(positions["colab"])
     # speed_moveL(positions["colab"])
+
+    # pickuptime = 1
+    # time limit
+
+    # # AUTONOMY TEST
+    # speed_moveL(positions["standby"])
+    # target = pose.copy
+    # target[1] = bottle.pos[1]
+
+    # speed_moveL(target, vacuum=True)
+
+    # while ( time_til_pickup - pickup_time > 0):
+    #     time.sleep(0.2)
+
 
     # # CHECK BOX LIMITS
     # position = bounding_box["max"]
@@ -162,91 +221,3 @@ if __name__ == '__main__':
     # target[2] = bounding_box["min"][2]
 
     # speed_moveL(target)
-
-# time_to_grab = 3
-# z_target = 0
-# speed = 2
-# Kp = 1
-# delay = 1
-
-
-
-# class Position:
-#     def __init__(self, x, y, z):
-#         self.x = x
-#         self.y = y
-#         self.z = z
-    
-#     def ls(self):
-#         return [self.x, self.y, self.z]
-
-# class Velocity:
-#     def __init__(self, x, y, z):
-#         self.x = x
-#         self.y = y
-#         self.z = z
-
-#     def ls(self):
-#         return [self.x, self.y, self.z]
-
-# class Bottle:
-#     def __init__(self, pos, vel, color):
-#         self.pos = pos
-#         self.vel = vel
-#         self.color = color
-    
-#     def future_position(self, time_delay):
-#         return [self.pos.x + self.vel.x* time_delay,
-#                 self.pos.y + self.vel.y*time_delay,
-#                 self.pos.z,
-#                 0,0,0]
-
-
-# drop_positions = {"blue": Position(-0.13163, -794.96,-319.50),
-#                   "green": Position(0.15800,-805.99,-319.50),
-#                   "red": Position(-440.52,-799.56,-319.50)}
-
-
-
-
-# while(True):
-#     bottle = get_target()
-
-#     target_pos = bottle.future_position(time_delay)
-#     cur_pos = rtde_r.getActualTCPPose()
-#     TIME_SPAN = speed_moveL(cur_pos, target_pos, speed, Kp)
-#     wait(time_to_grab - TIME_SPAN - delay)
-
-#     speedL(DOWN, speed)
-#     vacuum_on()
-
-#     while sense_bottle():
-#         cur_pos = rtde_r.getActualTCPPose()
-#         target_pos = target_logic(bottle.color)
-#         speed_moveL(cur_pos, target_pos, speed, Kp)
-#         vacuum_off()
-
-#     moveL(standby,0,0)
-
-    
-
-        
-
-
-"""
-- Get bottle data (position, velocity, color)
-- Calculate the intersection point
-- Move to intersection point
-- Activate vacuum
-- Lift
-- Find if bottle is attached
-if attached:
-    find color
-    move to endpoint
-    release vacuum
-    move to standby
-
-if not attached:
-    move to standby
-
-"""
